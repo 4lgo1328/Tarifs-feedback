88df0481c88ec4e0849855d01a7a7d8e13dfaf03
ekcne <ekcnert1337@icloud.com>
2025-01-20 22:32:20 +0300

    minor fixes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import FeedbackForm
 from werkzeug.utils import secure_filename
 from app.models import Feedback
@@ -8,30 +8,27 @@ from app.models import Feedback
 def init_routes(app, db):
     @app.route('/feedback', methods=['GET', "POST"])
     def upload():
-        form = FeedbackForm()
-        if form.validate_on_submit():
-            user_name = form.username.data
-            feedback_text = form.feedback.data
-            picture_file = form.picture.data
+        if request.method == "POST":
+            form = FeedbackForm()
+            if form.validate_on_submit():
+                user_name = form.username.data
+                feedback_text = form.feedback.data
+                picture_file = form.picture.data
 
-            filename = secure_filename(picture_file.filename)
-            picture_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-            print(picture_file)
-            print(picture_path)
-            picture_file.save(picture_path)
-            feedback_entry = Feedback(name=user_name, photo=filename, feedback=feedback_text)
-            print('до дб все ок')
-            db.session.add(feedback_entry)
-            db.session.commit()
-            flash('Ваш отзыв отправлен на рассмотрение администраторам!')
-            return redirect(url_for('main'))
-        else:
-            print("Form is invalid!")
-            print(form.errors)
-
+                filename = secure_filename(picture_file.filename)
+                picture_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+                picture_file.save(picture_path)
+                feedback_entry = Feedback(name=user_name, photo=filename, feedback=feedback_text)
+                db.session.add(feedback_entry)
+                db.session.commit()
+                flash('Ваш отзыв отправлен на рассмотрение администраторам!')
+                return redirect(url_for('main'))
+            else:
+                flash('Проверьте правильность ввода данных')
 
         return render_template('feedback.html', form=form)
 
     @app.route('/')
+    @app.route('/main/')
     def main():
         return render_template('index.html')
--- bot.py
+++ bot.py
@@ -1,20 +1,29 @@
-# import telebot
-# import config
-# from telebot import types
-#
-# bot = telebot.TeleBot(token=config.TOKEN)
-#
-# def get_markup(UID):
-#     mk = types.InlineKeyboardMarkup(
-#         types.InlineKeyboardButton(text='✔️', callback_data=f'accept_{UID}'),
-#         types.InlineKeyboardButton(text='❌', callback_data=f'decline_{UID}')
-#     )
-#     return mk
-# def send_feedback(UID, user_name, photo, feedback):
-#     bot.send_message(config.ADMIN_ID,
-#                      text=f'Новый отзыв!\n'
-#                           f'<u>Имя:</u> {user_name}\n'
-#                           f'<u>Отзыв:</u> {feedback}'
-#                           f'<u>Скриншот покупки</u> {photo}',
-#                      parse_mode='HTML',
-#                      reply_markup=get_markup(UID))
+import telebot
+import config
+from telebot import types
+
+bot = telebot.TeleBot(token=config.TOKEN)
+
+
+def get_markup(UID):
+    mk = types.InlineKeyboardMarkup(
+        types.InlineKeyboardButton(text='✔️', callback_data=f'accept_{UID}'),
+        types.InlineKeyboardButton(text='❌', callback_data=f'decline_{UID}')
+    )
+    return mk
+
+
+def send_feedback(UID, user_name, photo, feedback): # TODO
+    bot.send_message(config.ADMIN_ID,
+                     text=f'Новый отзыв!\n'
+                          f'<u>Имя:</u> {user_name}\n'
+                          f'<u>Отзыв:</u> {feedback}'
+                          f'<u>Скриншот покупки</u> {photo}',
+                     parse_mode='HTML',
+                     reply_markup=get_markup(UID))
+
+
+def parse_updates():
+    @bot.callback_query_handler()
+
+    bot.infinity_polling()
