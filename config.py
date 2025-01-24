e380ad895a5a00a201a4681f9d0029eaf5885525
ekcne <ekcnert1337@icloud.com>
2025-01-24 13:03:15 +0300

    small bugfixes

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    WTF_CSRF_ENABLED = True
-    SECRET_KEY = os.urandom(24)
+    SESSION_PERMANENT = False
+    SECRET_KEY = "secret-key"
     SESSION_TYPE = 'filesystem'
     DEBUG = os.getenv('FLASK_DEBUG', True)
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
@@ -11,11 +11,13 @@ class Config:
     MAX_CONTENT_LENGTH = 15 * 1024 * 1024
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # UPLOAD_FOLDER = 'home/c/cs37164/public_html/uploads' # -- for linux
 
 
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = 860597138
 SITE_PATH = 'instance/site.db'
 APPROVED_PATH = 'instance/approved.db'
-PATH_TO_UPLOADS = "../uploads/"
-# TODO: slash in the end of the path to uploads
+PATH_TO_UPLOADS = "http://localhost/uploads/"
+# TODO: absolute path in the uploads
+# TODO MAKE SYSTEM SAVE FILE IN STATIC DIRECTORY
